636155e6c934fa01874ca923e28505d25a7c584d
Oliver van den Bosch <74139069+ovandenbosch@users.noreply.github.com>
2021-02-26 10:24:50 -0500

    Messaging test

---
+++ bot.py
@@ -0,0 +1,12 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Loading environment variable
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
+client.chat_postMessage(channel='#test', text="I need to go now")
